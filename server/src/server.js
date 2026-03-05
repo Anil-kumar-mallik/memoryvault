e0733e89b1167dda5605ec2f08076785ce12777a
ANIL KUMAR MALLIK <221402486+Anil-kumar-mallik@users.noreply.github.com>
2026-03-05 08:53:17 +0530

    stabilize gridfs initialization

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 const connectDB = require("./config/db");
+const { getImageFile } = require("./config/gridfs");
 const validateEnv = require("./config/validateEnv");
 const logger = require("./utils/logger");
 const { ensureConfiguredPlans } = require("./utils/subscriptionService");
@@ -12,6 +13,8 @@ const PORT = Number(process.env.PORT) || 5000;
 const startServer = async () => {
   validateEnv();
   await connectDB();
+  /* Ensure GridFS bucket initializes after MongoDB connection */
+  require("./config/gridfs");
   await ensureConfiguredPlans();
   logger.info("Connected DB Name", { dbName: mongoose.connection.name });
 
